b49369b5f3c29986160f4437e2969cd3b827c724
Ferigatr <81425184+Ferigatr@users.noreply.github.com>
2021-05-31 14:48:58 -0300

    parte do repositório que não foi para o git
    o problema dessa versão é que não foi algumas partes finais do arquivo

---
+++ cafeteriaAtualizada/src/main/java/br/com/serratec/cafeteria/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package br.com.serratec.cafeteria.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.serratec.cafeteria.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	Optional<Cliente> findById(Long idCliente);
+
+	Optional<Cliente> findByName(String nome);
+
+}
